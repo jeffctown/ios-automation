a95e17b917281112271e155befd344f54f45f597
Jeff Lett <jeffctown@gmail.com>
2017-08-14 20:47:13 -0400

    Slack post script working.

--- swift/post-to-slack.swift
+++ swift/post-to-slack.swift
@@ -1,53 +1,62 @@
 import Foundation
 import Moderator
-import SwiftShell
+
+
+class RunLoop {
+    static var dispatchSemaphore = DispatchSemaphore(value: 0)
+}
 
 let arguments = Moderator(description: "Post a message to Slack.")
 
-let messageArg = arguments.add(Argument<String?>.optionWithValue("m", name: "message", description: "The message to post."))
+let messageArg = arguments.add(Argument<String>.optionWithValue("m", name: "message", description: "The message to post.").required())
+let urlSlugArg = arguments.add(Argument<String>.optionWithValue("p", name: "path", description: "The url path (the part after https://hooks.slack.com/services/).").required())
 
 do {
     try arguments.parse()
     
-    print("*** MessageArg.value: \(messageArg.value ?? "nil")")
+    print("*** Constructing Slack URL.")
+    let urlString = "https://hooks.slack.com/services/" + urlSlugArg.value
+    let url = URL(string: urlString)!
+    print("*** Done Constructing Slack URL: (\(url.absoluteString))")
     
-    guard let message = messageArg.value else {
-        throw ArgumentError(errormessage: "Slack message not found.", usagetext: "use -m Message")
-    }
+    print("*** Constructing JSON Body.")
+    let json = "{\"text\":\"\(messageArg.value)\"}"
+    print("*** Done Constructing JSON Body. (\(json))")
     
-    print("*** Message: \(message)")    
+    // url session & configuration
+    let configuration = URLSessionConfiguration.default
+    let urlSession = URLSession(configuration: configuration)
+    
+    // url request
+    var urlRequest = URLRequest(url: url, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 5.0)
+    urlRequest.httpMethod = "POST"
+    
+    // post body
+    let bodyData = json.data(using: String.Encoding.utf8, allowLossyConversion: true)
+    print("*** HTTP Body: \(bodyData!)")
+    urlRequest.httpBody = bodyData
+    
+    // do it now
+    print("*** Posting Message to Slack.")
+    urlSession.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
+        print("*** Done Posting Message to Slack.")
+        
+        if let httpResponse = response as? HTTPURLResponse {
+            print("*** ResponseCode: \(httpResponse.statusCode)")
+        }
+        
+        if let error = error {
+            print("*** Error encountered: \(error)")
+            exit(Int32(error._code))
+        }
+        
+        RunLoop.dispatchSemaphore.signal()
+    }).resume()
+    
+    RunLoop.dispatchSemaphore.wait()
     
 } catch {
     print("*** FAILURE!")
     print(error)
     exit(Int32(error._code))
 }
-
-
-//xcode-server user top-build channel
-//T063JHF8T/B57H8R62F/jeAKWiwzpvxpBQqYJ40kSuUG
-//automation bot user ios-automation channel
-//T063JHF8T/B34MRS0KB/hUY204wbpnmulJUkuBHN0EC8
-
-//API_URL=https://hooks.slack.com/services/
-
-//let integrationResultArg = arguments.add(Argument<String?>.optionWithValue("x", name: "xcoderesult", description: "The integration result from Xcode Server."))
-//let errorsArg = arguments.add(Argument<String?>.optionWithValue("e", name: "errors", description: "The number of build errors."))
-//let warningsArg = arguments.add(Argument<String?>.optionWithValue("w", name: "warnings", description: "The number of build warnings."))
-
-
-//    guard let urlSlug = urlSlugArg.value else {
-//        throw ArgumentError(errormessage: "Slack URL path not found.", usagetext: "use -p T000000/B000000/XXXXXXX")
-//    }
-//
-//    guard let botName = botNameArg.value else {
-//        throw ArgumentError(errormessage: "Bot name not found.", usagetext: "use -n BotName")
-//    }
-
-
-//let botNameArg = arguments.add(Argument<String?>.optionWithValue("n", name: "name", description: "The name of the bot."))
-//let urlSlugArg = arguments.add(Argument<String>.optionWithValue("p", name: "path", description: "The url path (the part after https://hooks.slack.com/services/)."))
-
-//    print("*** Constructing Slack URL.")
-//    let apiUrl = "https://hooks.slack.com/services/" + urlSlug
-//    print("*** Done constructing Slack URL. (\(apiUrl))")
